1fd911e1825ac91725f634f5443844feaddbe8f0
Christelle Giga <christellebento@hotmail.com>
2024-05-30 09:00:03 +0200

    fix:add cross origin

--- src/main/java/com/example/My/service/skills/controller/SkillController.java
+++ src/main/java/com/example/My/service/skills/controller/SkillController.java
@@ -1,6 +1,7 @@
 package com.example.My.service.skills.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200/")
 public class SkillController {
 
     SkillServiceImpl skillService;
